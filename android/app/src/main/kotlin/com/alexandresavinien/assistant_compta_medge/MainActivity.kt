395f6e20fb85c44d3e57005f64e79a5b9ac991ee
unknown <alexandresavinien@gmail.com>
2021-02-08 16:25:59 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/alexandresavinien/assistant_compta_medge/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alexandresavinien.assistant_compta_medge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/all.dart';
+
+void main() {
+  runApp(
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
